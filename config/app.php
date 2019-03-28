7d429344c606504c56cdc71dcea3426e98da1f76
jonaselan <jonaselan@rits.com.br>
2019-03-28 11:54:08 -0300

    Try to authenticate laravel echo requests

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
         /*
          * Application Service Providers...
